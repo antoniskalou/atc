9aee5d2612fedf98fdaf365e34e38c79c9f242e1
Antonis Kalou <kalouantonis@protonmail.com>
2022-03-16 16:00:07 +0200

    Add cmd line and callsign encoding

---
+++ src/aircraft.rs
@@ -0,0 +1,222 @@
+use ggez::{graphics::{self, Color}, Context, GameResult};
+use crate::geom::*;
+
+#[derive(Clone, Debug)]
+pub struct AircraftDefinition {
+    max_speed: u32,
+    min_speed: u32,
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub enum AircraftStatus {
+    Taxi,
+    Takeoff,
+    Landing,
+    Landed,
+    Flight,
+}
+
+// only encodes flight callsigns, not aircraft
+#[derive(Clone, Debug, PartialEq)]
+pub struct Callsign {
+    pub name: String,
+    pub code: String,
+    pub number: String,
+}
+
+impl Callsign {
+    pub fn coded(&self) -> String {
+        format!("{}{}", self.code, self.number)
+    }
+
+    pub fn spoken(&self) -> String {
+        format!("{} {}", self.name, self.number)
+    }
+}
+
+impl std::fmt::Display for Callsign {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "{}", self.coded())
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct Aircraft {
+    pub position: Point,
+    pub callsign: Callsign,
+    // bearing
+    pub heading: i32,
+    /// feet
+    pub altitude: u32,
+    /// knots
+    pub speed: u32,
+    pub on_ils: Option<ILS>,
+    pub status: AircraftStatus,
+}
+
+impl Aircraft {
+    pub fn change_heading(&mut self, new_course: i32) {
+        self.heading = if new_course < 0 {
+            360
+        } else if new_course > 360 {
+            0
+        } else {
+            new_course
+        };
+    }
+
+    pub fn change_altitude(&mut self, new_altitude: u32) {
+        self.altitude = new_altitude.max(1000);
+    }
+
+    pub fn change_speed(&mut self, new_speed: u32) {
+        // TODO: depends on aircraft type
+        self.speed = new_speed.clamp(150, 250);
+    }
+
+    pub fn is_localizer_captured(&self, localizer: &ILS) -> bool {
+        is_point_in_triangle(self.position, localizer.as_triangle()) &&
+            self.altitude <= localizer.altitude(self.position)
+    }
+
+    pub fn is_grounded(&self) -> bool {
+        self.status == AircraftStatus::Taxi || self.status == AircraftStatus::Landed
+    }
+
+    pub fn cleared_to_land(&self) -> bool {
+        self.status == AircraftStatus::Landing
+    }
+}
+
+pub const ILS_LENGTH: f32 = 300.0;
+
+#[derive(Clone, Debug)]
+pub struct ILS {
+    // position at end of the runway
+    origin: Point,
+    runway: Runway,
+}
+
+impl ILS {
+    pub fn as_triangle(&self) -> Vec<Point> {
+        let localizer = [
+            self.origin,
+            // 3 degree variance
+            rotate_point(
+                self.origin,
+                Point {
+                    x: self.origin.x,
+                    y: self.origin.y + ILS_LENGTH,
+                },
+                -3f32.to_radians(),
+            ),
+            rotate_point(
+                self.origin,
+                Point {
+                    x: self.origin.x,
+                    y: self.origin.y + ILS_LENGTH,
+                },
+                3f32.to_radians(),
+            ),
+        ];
+
+        rotate_points(
+            self.origin,
+            &localizer,
+            (self.runway.heading as f32).to_radians(),
+        )
+    }
+
+    pub fn distance(&self, position: Point) -> f32 {
+        point_distance(position, self.origin)
+    }
+
+    pub fn altitude(&self, position: Point) -> u32 {
+        let distance = self.distance(position);
+        let expected_alt = self.runway.ils_max_altitude as f32 * (distance / ILS_LENGTH);
+        // round to 1000
+        let rounded_alt = (expected_alt / 1000.0).round() * 1000.0;
+        rounded_alt as u32
+    }
+}
+
+const RUNWAY_DOWNSCALE: f32 = 10.0;
+
+#[derive(Clone, Debug)]
+pub struct Runway {
+    /// offset from airport
+    pub offset: Point,
+    /// bearing
+    pub heading: u32,
+    /// length in meters
+    pub length: u32,
+    /// width in meters
+    pub width: u32,
+    /// in feet
+    pub ils_max_altitude: u32,
+}
+
+impl Runway {
+    pub fn as_line(&self, origin: Point) -> Vec<Point> {
+        rotate_points(
+            origin,
+            &[
+                Point {
+                    x: origin.x,
+                    y: origin.y - (self.length as f32 / RUNWAY_DOWNSCALE / 2.0),
+                },
+                Point {
+                    x: origin.x,
+                    y: origin.y + (self.length as f32 / RUNWAY_DOWNSCALE / 2.0),
+                },
+            ],
+            (self.heading as f32).to_radians(),
+        )
+    }
+
+    pub fn ils(&self, origin: Point) -> ILS {
+        let origin = Point {
+            // rotated runway line points
+            x: self.as_line(origin)[1].x,
+            y: self.as_line(origin)[1].y,
+        };
+        // note, state not automatically updated
+        ILS { origin, runway: self.clone(), }
+    }
+
+    // TODO
+    pub fn has_landed(&self, origin: Point, aircraft: &Aircraft) -> bool {
+        is_point_in_circle(aircraft.position, origin, 10.0)
+    }
+
+    pub fn as_mesh(
+        &self,
+        ctx: &mut Context,
+        origin: Point,
+        color: Color,
+    ) -> GameResult<graphics::Mesh> {
+        graphics::Mesh::new_line(
+            ctx,
+            &self.as_line(origin),
+            self.width as f32 / RUNWAY_DOWNSCALE,
+            color,
+        )
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct Airport {
+    pub position: Point,
+    pub icao_code: String,
+    pub takeoff_runways: Vec<Runway>,
+    pub landing_runways: Vec<Runway>,
+}
+
+impl Airport {
+    pub fn origin(&self, runway: &Runway) -> Point {
+        Point {
+            x: self.position.x + runway.offset.x,
+            y: self.position.y + runway.offset.y,
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+mod aircraft;
 mod geom;
 mod tts;
 
@@ -7,6 +8,38 @@ use ggez::{
     timer, Context, ContextBuilder, GameResult,
 };
 use crate::geom::*;
+use crate::aircraft::*;
+
+use std::sync::mpsc;
+use std::io::Write;
+
+#[derive(Debug)]
+struct CliPrompt {
+    thread: std::thread::JoinHandle<()>,
+    receiver: std::sync::mpsc::Receiver<String>,
+}
+
+impl CliPrompt {
+    pub fn new() -> Self {
+        let (tx, rx): (mpsc::Sender<String>, mpsc::Receiver<String>) = mpsc::channel();
+
+        let thread = std::thread::spawn(move || {
+            loop {
+                let mut line = String::new();
+                print!("> ");
+                std::io::stdout().flush().unwrap();
+                std::io::stdin().read_line(&mut line).expect("failed to read stdin line");
+                tx.send(line).unwrap();
+            }
+        });
+
+        Self { thread, receiver: rx, }
+    }
+
+    pub fn try_input(&self) -> Option<String> {
+        self.receiver.try_recv().ok()
+    }
+}
 
 const TTS_ENABLED: bool = false;
 
@@ -16,8 +49,8 @@ struct ATC {
 }
 
 impl ATC {
-    fn new() -> ATC {
-        ATC {
+    fn new() -> Self {
+        Self {
             tts: if TTS_ENABLED {
                 Some(tts::TextToSpeech::new())
             } else { 
@@ -28,10 +61,10 @@ impl ATC {
 
     fn command(&mut self, aircraft: &mut Aircraft, cmd: ATCCommand) {
         // request
-        println!("{}, ATC, {}", aircraft.callsign, cmd.as_string());
+        println!("{}, {}", aircraft.callsign, cmd.as_string());
         if let Some(tts) = &mut self.tts {
             tts
-                .say(format!("{}, ATC, {}", callsign_to_name(&aircraft.callsign), cmd.as_string()))
+                .say(format!("{}, {}", aircraft.callsign.spoken(), cmd.as_string()))
                 .expect("failed to send tts message");
         }
 
@@ -64,12 +97,13 @@ enum ATCCommand {
 
 impl ATCCommand {
     fn from_string(s: String) -> Vec<ATCCommand> {
-        let command_parts: Vec<&str> = s.split(' ').collect();
+        let command_parts: Vec<&str> = s.trim().split(' ').collect();
     
         let mut commands = Vec::new();
         let mut iter = command_parts.iter();
         loop {
             let cmd = iter.next()
+                // .filter(|x| x.is_empty())
                 .map(|x| x.to_uppercase())
                 .map(|x| match x.as_str() {
                     "LND" => ATCCommand::ClearedToLand(true), 
@@ -117,223 +151,12 @@ impl ATCCommand {
 struct ATCRequest(ATCCommand);
 struct ATCReply(ATCCommand);
 
-// FIXME: crappy way to get callsign
-fn callsign_to_name(callsign: &String) -> String {
-    let (icao_code, flight_number) = callsign.split_at(3);
-    let spoken_callsign = 
-        match icao_code {
-            "TRA" => "Transavia",
-            "CYP" => "Cyprus Airways",
-            _other => {
-                // TODO: use phonetic alphabet
-                "unknown"
-            },
-        };
-
-    format!("{} {}", spoken_callsign, flight_number)
-}
-
-#[derive(Clone, Debug)]
-struct AircraftDefinition {
-    max_speed: u32,
-    min_speed: u32,
-}
-
-#[derive(Clone, Debug, PartialEq)]
-enum AircraftStatus {
-    Taxi,
-    Takeoff,
-    Landing,
-    Landed,
-    Flight,
-}
-
-#[derive(Clone, Debug)]
-struct Aircraft {
-    position: Point,
-    callsign: String,
-    // bearing
-    heading: i32,
-    /// feet
-    altitude: u32,
-    /// knots
-    speed: u32,
-    on_ils: Option<ILS>,
-    status: AircraftStatus,
-}
-
 const AIRCRAFT_RADIUS: f32 = 4.0;
 const AIRCRAFT_BOUNDING_RADIUS: f32 = AIRCRAFT_RADIUS * 5.0;
 
-impl Aircraft {
-    pub fn change_heading(&mut self, new_course: i32) {
-        self.heading = if new_course < 0 {
-            360
-        } else if new_course > 360 {
-            0
-        } else {
-            new_course
-        };
-    }
-
-    pub fn change_altitude(&mut self, new_altitude: u32) {
-        self.altitude = new_altitude.max(1000);
-    }
-
-    pub fn change_speed(&mut self, new_speed: u32) {
-        // TODO: depends on aircraft type
-        self.speed = new_speed.clamp(150, 250);
-    }
-
-    fn is_localizer_captured(&self, localizer: &ILS) -> bool {
-        is_point_in_triangle(self.position, localizer.as_triangle()) &&
-            self.altitude <= localizer.altitude(self.position)
-    }
-
-    fn is_grounded(&self) -> bool {
-        self.status == AircraftStatus::Taxi || self.status == AircraftStatus::Landed
-    }
-
-    fn cleared_to_land(&self) -> bool {
-        self.status == AircraftStatus::Landing
-    }
-}
-
-const ILS_LENGTH: f32 = 300.0;
-
-#[derive(Clone, Debug)]
-struct ILS {
-    // position at end of the runway
-    origin: Point,
-    runway: Runway,
-}
-
-impl ILS {
-    fn as_triangle(&self) -> Vec<Point> {
-        let localizer = [
-            self.origin,
-            // 3 degree variance
-            rotate_point(
-                self.origin,
-                Point {
-                    x: self.origin.x,
-                    y: self.origin.y + ILS_LENGTH,
-                },
-                -3f32.to_radians(),
-            ),
-            rotate_point(
-                self.origin,
-                Point {
-                    x: self.origin.x,
-                    y: self.origin.y + ILS_LENGTH,
-                },
-                3f32.to_radians(),
-            ),
-        ];
-
-        rotate_points(
-            self.origin,
-            &localizer,
-            (self.runway.heading as f32).to_radians(),
-        )
-    }
-
-    fn distance(&self, position: Point) -> f32 {
-        point_distance(position, self.origin)
-    }
-
-    fn altitude(&self, position: Point) -> u32 {
-        let distance = self.distance(position);
-        let expected_alt = self.runway.ils_max_altitude as f32 * (distance / ILS_LENGTH);
-        // round to 1000
-        let rounded_alt = (expected_alt / 1000.0).round() * 1000.0;
-        rounded_alt as u32
-    }
-}
-
-const RUNWAY_DOWNSCALE: f32 = 10.0;
-
-#[derive(Clone, Debug)]
-struct Runway {
-    /// offset from airport
-    offset: Point,
-    /// bearing
-    heading: u32,
-    /// length in meters
-    length: u32,
-    /// width in meters
-    width: u32,
-    /// in feet
-    ils_max_altitude: u32,
-}
-
-impl Runway {
-    pub fn as_line(&self, origin: Point) -> Vec<Point> {
-        rotate_points(
-            origin,
-            &[
-                Point {
-                    x: origin.x,
-                    y: origin.y - (self.length as f32 / RUNWAY_DOWNSCALE / 2.0),
-                },
-                Point {
-                    x: origin.x,
-                    y: origin.y + (self.length as f32 / RUNWAY_DOWNSCALE / 2.0),
-                },
-            ],
-            (self.heading as f32).to_radians(),
-        )
-    }
-
-    fn ils(&self, origin: Point) -> ILS {
-        let origin = Point {
-            // rotated runway line points
-            x: self.as_line(origin)[1].x,
-            y: self.as_line(origin)[1].y,
-        };
-        // note, state not automatically updated
-        ILS { origin, runway: self.clone(), }
-    }
-
-    // TODO
-    fn has_landed(&self, origin: Point, aircraft: &Aircraft) -> bool {
-        is_point_in_circle(aircraft.position, origin, 10.0)
-    }
-
-    pub fn as_mesh(
-        &self,
-        ctx: &mut Context,
-        origin: Point,
-        color: Color,
-    ) -> GameResult<graphics::Mesh> {
-        graphics::Mesh::new_line(
-            ctx,
-            &self.as_line(origin),
-            self.width as f32 / RUNWAY_DOWNSCALE,
-            color,
-        )
-    }
-}
-
-#[derive(Clone, Debug)]
-struct Airport {
-    position: Point,
-    icao_code: String,
-    takeoff_runways: Vec<Runway>,
-    landing_runways: Vec<Runway>,
-}
-
-impl Airport {
-    fn origin(&self, runway: &Runway) -> Point {
-        Point {
-            x: self.position.x + runway.offset.x,
-            y: self.position.y + runway.offset.y,
-        }
-    }
-}
-
 #[derive(Debug)]
 struct Game {
+    cli: CliPrompt,
     atc: ATC,
     airports: Vec<Airport>,
     selected_aircraft: usize,
@@ -351,6 +174,7 @@ impl Game {
         };
 
         Self {
+            cli: CliPrompt::new(),
             atc: ATC::new(),
             airports: vec![Airport {
                 position: Point { x: 500.0, y: 550.0 },
@@ -362,7 +186,11 @@ impl Game {
             aircraft: vec![
                 Aircraft {
                     position: ggez::mint::Point2 { x: 250.0, y: 200.0 },
-                    callsign: "CYP2202".into(),
+                    callsign: Callsign {
+                        name: "Cyprus Airways".into(),
+                        code: "CYP".into(),
+                        number: "2202".into(),
+                    },
                     heading: 90,
                     altitude: 6000,
                     speed: 250,
@@ -371,7 +199,11 @@ impl Game {
                 },
                 Aircraft {
                     position: ggez::mint::Point2 { x: 500.0, y: 400.0 },
-                    callsign: "TRA1112".into(),
+                    callsign: Callsign {
+                        name: "Transavia".into(),
+                        code: "TRA".into(),
+                        number: "1112".into(),
+                    },
                     heading: 180,
                     altitude: 12000,
                     speed: 220,
@@ -386,6 +218,12 @@ impl Game {
 impl EventHandler<ggez::GameError> for Game {
     fn update(&mut self, ctx: &mut Context) -> GameResult<()> {
         let dt = timer::delta(ctx);
+
+        if let Some(msg) = self.cli.try_input() {
+            for cmd in ATCCommand::from_string(msg) {
+                self.atc.command(&mut self.aircraft[self.selected_aircraft], cmd);
+            }
+        }
 
         for mut aircraft in &mut self.aircraft {
             if !aircraft.is_grounded() {
@@ -437,29 +275,7 @@ impl EventHandler<ggez::GameError> for Game {
         _keymod: event::KeyMods,
         _repeat: bool,
     ) {
-        let aircraft = &mut self.aircraft[self.selected_aircraft];
-
-        if keycode == KeyCode::A {
-            let new_heading = aircraft.heading - 5;
-            self.atc.command(aircraft, ATCCommand::ChangeHeading(new_heading));
-        } else if keycode == KeyCode::D {
-            let new_heading = aircraft.heading + 5;
-            self.atc.command(aircraft, ATCCommand::ChangeHeading(new_heading));
-        } else if keycode == KeyCode::S {
-            let new_alt = aircraft.altitude - 1000;
-            self.atc.command(aircraft, ATCCommand::ChangeAltitude(new_alt));
-        } else if keycode == KeyCode::W {
-            let new_alt = aircraft.altitude + 1000;
-            self.atc.command(aircraft, ATCCommand::ChangeAltitude(new_alt));
-        } else if keycode == KeyCode::F {
-            let new_speed = aircraft.speed - 10;
-            self.atc.command(aircraft, ATCCommand::ChangeSpeed(new_speed));
-        } else if keycode == KeyCode::R {
-            let new_speed = aircraft.speed + 10;
-            self.atc.command(aircraft, ATCCommand::ChangeSpeed(new_speed));
-        } else if keycode == KeyCode::L {
-            self.atc.command(aircraft, ATCCommand::ClearedToLand(!aircraft.cleared_to_land()));
-        } else if keycode == KeyCode::LBracket {
+        if keycode == KeyCode::LBracket {
             self.selected_aircraft = (self.selected_aircraft as i32 - 1).max(0) as usize;
         } else if keycode == KeyCode::RBracket {
             self.selected_aircraft = (self.selected_aircraft + 1).min(self.aircraft.len() - 1);
@@ -535,7 +351,7 @@ impl EventHandler<ggez::GameError> for Game {
 
             graphics::draw(ctx, &bounding_circle, (Point { x: 0.0, y: 0.0 },))?;
 
-            let callsign_text = graphics::Text::new(aircraft.callsign.clone());
+            let callsign_text = graphics::Text::new(aircraft.callsign.coded());
             graphics::queue_text(
                 ctx,
                 &callsign_text,
